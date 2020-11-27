38603fce3ab20596f5a79886df37dec5ccdf9d98
Guilherme Ramos <gui_aramos@outlook.com>
2020-11-27 10:37:02 +0900

    :star: add(fundamentals): add reflection

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -34,6 +44,44 @@ func TestWalk(t *testing.T) {
 			}{"Gui", 30},
 			[]string{"Gui"},
 		},
+		{
+			"Nested fields",
+			Person{
+				"Gui",
+				Profile{27, "Seoul"},
+			},
+			[]string{"Gui", "Seoul"},
+		}, {
+			"Pointers to things",
+			&Person{
+				"Gui",
+				Profile{27, "Seoul"},
+			},
+			[]string{"Gui", "Seoul"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "Seoul"},
+				{34, "London"},
+			},
+			[]string{"Seoul", "London"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "Seoul"},
+				{34, "London"},
+			},
+			[]string{"Seoul", "London"},
+		}, {
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
@@ -47,5 +95,71 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "London"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "London"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "London"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "London"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expectred %+v to contain %q but it didn't", haystack, needle)
 	}
 }
